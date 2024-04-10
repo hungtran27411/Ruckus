e907e2948bdfaf9bb1e3e898361cb46ea7bbdfc2
Thelonious <thelonious06@gmail.com>
2024-04-10 10:41:15 -0700

    set up models, basic templates

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    profile_name = models.CharField(max_length=15)
+    bio = models.TextField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Post(models.Model):
+    content = models.TextField(max_length=256)
