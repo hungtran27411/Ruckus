5681ec30b3d8e2263c9a5d4dcd4799d5e4b0d560
Thelonious <thelonious06@gmail.com>
2024-04-10 16:55:42 -0700

    index function consolidated to home function, post create function also integrated to home function

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.post_index, name='home'),
+    path('', views.home, name='home'),
     path('accounts/singup/', views.signup, name='signup'),
-    path('posts/', views.PostCreate.as_view(), name='create')
+    
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,49 +1,50 @@
 from django.shortcuts import render, redirect
-from .models import Post
+from .models import Post, Profile
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import ProfileForm, PostForm
 # Create your views here.
 
 
 def home(request):
-	return render(request, 'home.html')
-
-
-def post_index(request):
-	posts = Post.objects.all()
-	return render(request, 'home.html', {
-		'posts': posts
-	})
+    posts = Post.objects.all()
+    if request.user.is_authenticated:  # Check if user is logged in
+        if request.method == 'POST':
+            form = PostForm(request.POST)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.profile = request.user.profile
+                post.save()
+                return redirect('home')
+        else:
+            form = PostForm()
+        return render(request, 'home.html', {'form': form, 'posts': posts})
+    else:
+        return render(request, 'home.html', {'posts': posts})
 
 
 def signup(request):
-	error_message = ''
-	if request.method == 'POST':
-		form = UserCreationForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request, user)
-			return redirect('home')
-		else:
-			error_message = 'Invalid signup'
-	form = UserCreationForm()
-	return render(request, 'registration/signup.html', {
-		'error_message': error_message,
-		'form': form
-	})
-
-
-class PostCreate(LoginRequiredMixin, CreateView):
-	model = Post
-	fields = ['content']
-
-	def form_valid(self, form):
-		
-		form.instance.user = self.request.user
-		
-		return super().form_valid(form)
-		
-	
+    error_message = ''
+    if request.method == 'POST':
+        user_form = UserCreationForm(request.POST)
+        profile_form = ProfileForm(request.POST)
+        if user_form.is_valid() and profile_form.is_valid():
+            user = user_form.save()
+            profile = profile_form.save(commit=False)
+            profile.user = user
+            profile.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid signup'
+    else:
+        user_form = UserCreationForm()
+        profile_form = ProfileForm()
+    return render(request, 'registration/signup.html', {
+        'error_message': error_message,
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
