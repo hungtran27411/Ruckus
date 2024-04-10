509e7e8ae0b53bae17eb309e71773f6efbd49f2b
Thelonious <thelonious06@gmail.com>
2024-04-10 12:57:16 -0700

    lunch commit

--- main_app/views.py
+++ main_app/views.py
@@ -2,18 +2,21 @@ from django.shortcuts import render, redirect
 from .models import Post
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html')
+	return render(request, 'home.html')
+
 
 def post_index(request):
-    posts = Post.objects.all()
-    return render(request, 'home.html', {
-        'posts': posts
-    })
-
+	posts = Post.objects.all()
+	return render(request, 'home.html', {
+		'posts': posts
+	})
 
 
 def signup(request):
@@ -31,3 +34,16 @@ def signup(request):
 		'error_message': error_message,
 		'form': form
 	})
+
+
+class PostCreate(LoginRequiredMixin, CreateView):
+	model = Post
+	fields = ['content']
+
+	def form_valid(self, form):
+		
+		form.instance.user = self.request.user
+		
+		return super().form_valid(form)
+		
+	
