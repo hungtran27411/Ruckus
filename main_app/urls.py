98f851a7143cdca5f81a8da2cb439fb7ff211fc2
Max L <max_lupton@icloud.com>
2024-04-11 10:42:39 -0700

    order post chronologically

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('accounts/singup/', views.signup, name='signup'),
+    path('accounts/signup/', views.signup, name='signup'),
     path('post/<int:pk>/update/', views.PostUpdate.as_view(), name='update_post'),
     path('post/<int:pk>/delete/', views.PostDelete.as_view(), name='delete_post'),
 
