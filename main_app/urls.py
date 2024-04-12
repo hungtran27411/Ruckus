9f787a22cf9a3b50aa0e28ccbeb9e3644fca6353
Max L <max_lupton@icloud.com>
2024-04-12 11:59:18 -0700

    added profile pic upload

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
     path('post/<int:pk>/update/', views.PostUpdate.as_view(), name='update_post'),
     path('post/<int:pk>/delete/', views.PostDelete.as_view(), name='delete_post'),
     path('profile/<int:profile_id>/', views.profile_detail, name='profile_detail'),
+    path('profile/<int:profile_id>/add_photo/', views.add_user_photo, name='add_user_photo'),
+
 ]
